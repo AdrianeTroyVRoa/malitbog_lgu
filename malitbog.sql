932fcebb932af03937a301415a6252a48eb33e27
mcapsss <161445277+mcapsss@users.noreply.github.com>
2024-04-19 08:42:11 +0800

    Add files via upload

---
+++ malitbog.sql
@@ -0,0 +1,172 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 19, 2024 at 02:40 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `malitbog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_commentmeta`
+--
+-- Error reading structure for table malitbog.wp_commentmeta: #1932 - Table 'malitbog.wp_commentmeta' doesn't exist in engine
+-- Error reading data for table malitbog.wp_commentmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_commentmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_comments`
+--
+-- Error reading structure for table malitbog.wp_comments: #1932 - Table 'malitbog.wp_comments' doesn't exist in engine
+-- Error reading data for table malitbog.wp_comments: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_comments`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_links`
+--
+-- Error reading structure for table malitbog.wp_links: #1932 - Table 'malitbog.wp_links' doesn't exist in engine
+-- Error reading data for table malitbog.wp_links: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_links`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_nextend2_image_storage`
+--
+-- Error reading structure for table malitbog.wp_nextend2_image_storage: #1932 - Table 'malitbog.wp_nextend2_image_storage' doesn't exist in engine
+-- Error reading data for table malitbog.wp_nextend2_image_storage: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_nextend2_image_storage`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_nextend2_section_storage`
+--
+-- Error reading structure for table malitbog.wp_nextend2_section_storage: #1932 - Table 'malitbog.wp_nextend2_section_storage' doesn't exist in engine
+-- Error reading data for table malitbog.wp_nextend2_section_storage: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_nextend2_section_storage`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_nextend2_smartslider3_generators`
+--
+-- Error reading structure for table malitbog.wp_nextend2_smartslider3_generators: #1932 - Table 'malitbog.wp_nextend2_smartslider3_generators' doesn't exist in engine
+-- Error reading data for table malitbog.wp_nextend2_smartslider3_generators: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_nextend2_smartslider3_generators`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_nextend2_smartslider3_sliders`
+--
+-- Error reading structure for table malitbog.wp_nextend2_smartslider3_sliders: #1932 - Table 'malitbog.wp_nextend2_smartslider3_sliders' doesn't exist in engine
+-- Error reading data for table malitbog.wp_nextend2_smartslider3_sliders: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_nextend2_smartslider3_sliders`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_nextend2_smartslider3_sliders_xref`
+--
+-- Error reading structure for table malitbog.wp_nextend2_smartslider3_sliders_xref: #1932 - Table 'malitbog.wp_nextend2_smartslider3_sliders_xref' doesn't exist in engine
+-- Error reading data for table malitbog.wp_nextend2_smartslider3_sliders_xref: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_nextend2_smartslider3_sliders_xref`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_nextend2_smartslider3_slides`
+--
+-- Error reading structure for table malitbog.wp_nextend2_smartslider3_slides: #1932 - Table 'malitbog.wp_nextend2_smartslider3_slides' doesn't exist in engine
+-- Error reading data for table malitbog.wp_nextend2_smartslider3_slides: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_nextend2_smartslider3_slides`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_options`
+--
+-- Error reading structure for table malitbog.wp_options: #1932 - Table 'malitbog.wp_options' doesn't exist in engine
+-- Error reading data for table malitbog.wp_options: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_options`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_postmeta`
+--
+-- Error reading structure for table malitbog.wp_postmeta: #1932 - Table 'malitbog.wp_postmeta' doesn't exist in engine
+-- Error reading data for table malitbog.wp_postmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_postmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_posts`
+--
+-- Error reading structure for table malitbog.wp_posts: #1932 - Table 'malitbog.wp_posts' doesn't exist in engine
+-- Error reading data for table malitbog.wp_posts: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_posts`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_termmeta`
+--
+-- Error reading structure for table malitbog.wp_termmeta: #1932 - Table 'malitbog.wp_termmeta' doesn't exist in engine
+-- Error reading data for table malitbog.wp_termmeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_termmeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_terms`
+--
+-- Error reading structure for table malitbog.wp_terms: #1932 - Table 'malitbog.wp_terms' doesn't exist in engine
+-- Error reading data for table malitbog.wp_terms: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_terms`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_term_relationships`
+--
+-- Error reading structure for table malitbog.wp_term_relationships: #1932 - Table 'malitbog.wp_term_relationships' doesn't exist in engine
+-- Error reading data for table malitbog.wp_term_relationships: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_term_relationships`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_term_taxonomy`
+--
+-- Error reading structure for table malitbog.wp_term_taxonomy: #1932 - Table 'malitbog.wp_term_taxonomy' doesn't exist in engine
+-- Error reading data for table malitbog.wp_term_taxonomy: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_term_taxonomy`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_usermeta`
+--
+-- Error reading structure for table malitbog.wp_usermeta: #1932 - Table 'malitbog.wp_usermeta' doesn't exist in engine
+-- Error reading data for table malitbog.wp_usermeta: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_usermeta`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `wp_users`
+--
+-- Error reading structure for table malitbog.wp_users: #1932 - Table 'malitbog.wp_users' doesn't exist in engine
+-- Error reading data for table malitbog.wp_users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `malitbog`.`wp_users`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
